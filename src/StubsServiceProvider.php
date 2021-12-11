3ed087a2d2113ea3a5a1aad24fc28263065f6676
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2021-12-11 20:24:07 +0100

    Update StubsServiceProvider.php

--- src/StubsServiceProvider.php
+++ src/StubsServiceProvider.php
@@ -10,16 +10,8 @@ class StubsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('stubs')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_stubs_table')
             ->hasCommand(StubsCommand::class);
     }
 }
