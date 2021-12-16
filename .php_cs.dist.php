91e76036340d0e7262018a7fa531e081866ea166
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2021-12-16 16:06:32 +0100

    PHP CS Fixer fix for no_spaces_around_offset

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -11,58 +11,62 @@ $finder = Symfony\Component\Finder\Finder::create()
     ->ignoreVCS(true);
 
 return ( new PhpCsFixer\Config() )
-    ->setRules([
-        '@PSR12' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sort_algorithm' => 'alpha'],
-        'braces' => [
-            'allow_single_line_anonymous_class_with_empty_body' => true,
-        ],
-        'no_unused_imports' => true,
-        'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline' => true,
-        'phpdoc_scalar' => true,
-        'unary_operator_spaces' => true,
-        'binary_operator_spaces' => true,
-        'phpdoc_single_line_var_spacing' => true,
-        'phpdoc_var_without_name' => true,
-        'class_attributes_separation' => [
-            'elements' => [
-                'method' => 'one',
+    ->setRules(
+        [
+            '@PSR12' => true,
+            'array_syntax' => ['syntax' => 'short'],
+            'ordered_imports' => ['sort_algorithm' => 'alpha'],
+            'braces' => [
+                'allow_single_line_anonymous_class_with_empty_body' => true,
             ],
-        ],
-        'method_argument_space' => [
-            'on_multiline' => 'ensure_fully_multiline',
-            'keep_multiple_spaces_after_comma' => true,
-        ],
-        'single_trait_insert_per_statement' => true,
-        'array_push' => true,
-        'backtick_to_shell_exec' => true,
-        'ereg_to_preg' => true,
-        'no_alias_language_construct_call' => true,
-        'no_mixed_echo_print' => true,
-        'pow_to_exponentiation' => true,
-        'random_api_migration' => true,
-        'set_type_to_cast' => true,
-        'array_indentation' => true,
-        'blank_line_before_statement' => [
-            'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try', 'do', 'exit', 'goto', 'if', 'switch', 'yield'],
-            // Check for support with if conditions and early returns in a function
-        ],
-        'compact_nullable_typehint' => true,
-        'heredoc_indentation' => true,
-        'line_ending' => true,
-        'method_chaining_indentation' => true, // Check for support with chained methods
-        'no_extra_blank_lines' =>
-            [
-                'tokens' => ['continue', 'curly_brace_block', 'extra', 'return', 'parenthesis_brace_block', 'square_brace_block', 'throw', 'use', 'switch', 'case', 'default',],
+            'no_unused_imports' => true,
+            'not_operator_with_successor_space' => true,
+            'trailing_comma_in_multiline' => true,
+            'phpdoc_scalar' => true,
+            'unary_operator_spaces' => true,
+            'binary_operator_spaces' => true,
+            'phpdoc_single_line_var_spacing' => true,
+            'phpdoc_var_without_name' => true,
+            'class_attributes_separation' => [
+                'elements' => [
+                    'method' => 'one',
+                ],
             ],
-        'no_spaces_around_offset' => [
-            ['inside', 'outside'],
-        ],
-        'no_trailing_whitespace' => true,
-        'types_spaces' => [
-            'space' => 'none',
-        ],
-    ])
+            'method_argument_space' => [
+                'on_multiline' => 'ensure_fully_multiline',
+                'keep_multiple_spaces_after_comma' => true,
+            ],
+            'single_trait_insert_per_statement' => true,
+            'array_push' => true,
+            'backtick_to_shell_exec' => true,
+            'ereg_to_preg' => true,
+            'no_alias_language_construct_call' => true,
+            'no_mixed_echo_print' => true,
+            'pow_to_exponentiation' => true,
+            'random_api_migration' => true,
+            'set_type_to_cast' => true,
+            'array_indentation' => true,
+            'blank_line_before_statement' => [
+                'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try', 'do', 'exit', 'goto', 'if', 'switch', 'yield'],
+                // Check for support with if conditions and early returns in a function
+            ],
+            'compact_nullable_typehint' => true,
+            'heredoc_indentation' => true,
+            'line_ending' => true,
+            'method_chaining_indentation' => true, // Check for support with chained methods
+            'no_extra_blank_lines' =>
+                [
+                    'tokens' => ['continue', 'curly_brace_block', 'extra', 'return', 'parenthesis_brace_block', 'square_brace_block', 'throw', 'use', 'switch', 'case', 'default',],
+                ],
+            'no_spaces_around_offset' => [
+                'positions' => [
+                    ['inside', 'outside'],
+                ],
+            ],
+            'no_trailing_whitespace' => true,
+            'types_spaces' => [
+                'space' => 'none',
+            ],
+        ]
+    )
     ->setFinder($finder);
